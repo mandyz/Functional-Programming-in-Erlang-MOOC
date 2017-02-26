c95e3a76fbdcbb9810729b6f17f9c92b7af537de
Mandy Zammit <zammit.mandy@gmail.com>
2017-02-26 21:06:08 +0100

    Adding tail recursive definition of fibonacci.

--- week1/fibonacci.erl
+++ week1/fibonacci.erl
@@ -1,5 +1,5 @@
 -module(fibonacci).
--export([fib/1]).
+-export([fib/1,fibTL/1]).
 
 fibVal(1) ->
   0;
@@ -12,3 +12,16 @@ fib(0) ->
   [];
 fib(N) when N >= 1 ->
   fib(N-1) ++ [fibVal(N)].
+
+% Tail recursive implementation of fibonacci.
+fibValTL(2,X,Y) ->
+  X+Y;
+fibValTL(N, X, Y) when x >= 2->
+  fibValTL(N-1, Y, X+Y).
+
+fibTL(0) ->
+  [0];
+fibTL(1) ->
+  [0,1];
+fibTL(N) ->
+  fibTL(N-1) ++ [fibValTL(N, 0, 1)].
