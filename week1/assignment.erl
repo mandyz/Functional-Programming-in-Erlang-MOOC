deafc36cab0c7f346d6a4a740683a5f9fa83235c
Mandy Zammit <zammit.mandy@gmail.com>
2017-02-27 07:48:30 +0100

    Adding assignment workout for week1.

---
+++ week1/assignment.erl
@@ -0,0 +1,43 @@
+-module(assignment).
+-export([perimeter/1,area/1,enclose/1,bits/1,bitsTL/1]).
+
+
+perimeter({circle, R}) ->
+  2 * math:pi() * R;
+perimeter({traingle, A, B, C}) ->
+  A + B + C;
+perimeter({rectangle, W, H}) ->
+  (W + H) * 2.
+
+
+area({circle, R}) ->
+  math:pi() * first:square(R);
+area({traingle, A, B, C}) ->
+  first:area(A,B,C); % Using Heron's formual (defined alreay in module `first`).
+area({rectangle, W, H}) ->
+  W * H.
+
+
+enclose({circle, R}) ->
+  {square, R*2, R*2};
+enclose(T = {traingle, A, _, _}) ->
+  H = (area(T) * 2) / A,
+  {rectangle, A, H};
+enclose(_X) -> % Smallest enclosing rectangle of a rectangle is area of the same rectangle.
+  _X.
+
+
+% Direct recursive defnition of bits function.
+bits(0) ->
+  0;
+bits(N) when N > 0 ->
+  bits(N div 2) + (N rem 2).
+
+% Tail recursive defnition of bits function.
+bitsTL(N) ->
+  bitsValTL(N,0).
+
+bitsValTL(0,B) ->
+  B;
+bitsValTL(N,B) when N > 0 ->
+  bitsValTL(N div 2, B + (N rem 2)).
